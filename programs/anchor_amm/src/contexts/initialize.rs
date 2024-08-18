6007b6be4898036930e7ba03cf773b1480152971
akasimo <akasimo@fastmail.com>
2024-08-18 14:42:21 +0000

    initialization implemented

--- programs/anchor_amm/src/contexts/initialize.rs
+++ programs/anchor_amm/src/contexts/initialize.rs
@@ -1,47 +1,39 @@
 use anchor_lang::prelude::*;
-use anchor_spl::{token::TokenAccount, token_interface::{Mint, TokenInterface}};
-
+// use anchor_spl::{associated_token::AssociatedToken, token::{Mint, Token, TokenAccount}};
+use anchor_spl::{
+    associated_token::AssociatedToken, 
+    token::Token, 
+    token_interface::{ Mint, TokenAccount }
+};
 use crate::state::Config;
 
 #[derive(Accounts)]
+#[instruction(seed: u64)]
 pub struct Initialize<'info> {
     #[account(mut)]
     signer: Signer<'info>,
 
-    #[account(
-        token_program::mint = mint_x,
-        token_program::authority = config,
-    )]
     mint_x: InterfaceAccount<'info, Mint>,
     mint_y: InterfaceAccount<'info, Mint>,
     
     #[account(
         init_if_needed,
         payer=signer,
-        mint::authority = config,
-        mint::freeze_authority = config,
-        mint::decimals = 6,
-        mint::token_program = token_program,
         seeds = [b"mint".as_ref(), config.key().as_ref()],
-        bump
+        bump,
+        mint::authority = config,
+        mint::decimals = 6,
+        // mint::freeze_authority = config,
+        // mint::token_program = token_program,
     )]
     mint_lp: InterfaceAccount<'info, Mint>,
-
-    #[account(
-        init,
-        payer=signer,
-        space = 8 + Config::INITSPACE,
-        seeds = [b"amm".as_ref(), mint_x.key().as_ref(), mint_y.key().as_ref(), seed.to_le_bytes().as_ref()],
-        bump
-    )]
-    config: Account<'info, Config>,
 
     #[account(
         init_if_needed,
         payer=signer,
         associated_token::mint = mint_x,
         associated_token::authority = config,
-        associated_token::token_program = token_program,
+        // associated_token::token_program = token_program,
     )]
     vault_x: InterfaceAccount<'info, TokenAccount>,    
     
@@ -50,25 +42,38 @@ pub struct Initialize<'info> {
         payer=signer,
         associated_token::mint = mint_x,
         associated_token::authority = config,
-        associated_token::token_program = token_program,
+        // associated_token::token_program = token_program,
     )]
     vault_y: InterfaceAccount<'info, TokenAccount>,
 
-    associated_token_program: Interface<'info, TokenInterface>,
-    token_program: Interface<'info, TokenInterface>,
+    // /// CHECK: This account is only used to sign. it doesn't contain SOL
+    // #[account(seeds = [b"auth"], bump)]
+    // pub auth: UncheckedAccount<'info>,
+
+    #[account(
+        init,
+        payer=signer,
+        space = 8 + Config::INIT_SPACE,
+        seeds = [b"amm".as_ref(), mint_x.key().as_ref(), mint_y.key().as_ref(), seed.to_le_bytes().as_ref()],
+        bump
+    )]
+    config: Account<'info, Config>,
+
+    associated_token_program: Program<'info, Token>,
+    token_program: Program<'info, AssociatedToken>,
     system_program: Program<'info, System>,
 }
 
 
 impl<'info> Initialize<'info> {
-    pub fn save_config(&mut self, seed: u64, fee: u16, bump:u8, lp_bump: u8) -> ProgramResult {
+    pub fn save_config(&mut self, seed: u64, fee: u16, bumps: &InitializeBumps) -> Result<()> {
         self.config.set_inner(Config {
             seed,
             fee,
             mint_x: self.mint_x.key(),
             mint_y: self.mint_y.key(),
-            lp_bump,
-            bump,
+            lp_bump: bumps.mint_lp,
+            bump: bumps.config,
         });
         Ok(())
     }
--- programs/anchor_amm/src/lib.rs
+++ programs/anchor_amm/src/lib.rs
@@ -1,5 +1,7 @@
 use anchor_lang::prelude::*;
+
 pub mod state;
+pub use state::*;
 pub mod contexts;
 pub use contexts::*;
 
@@ -12,43 +14,43 @@ pub mod anchor_amm {
     // Intialize the pool
     pub fn initialize(ctx: Context<Initialize>, seed: u64, fee:u16) -> Result<()> {
         // save config
-        ctx.accounts.save_config(seed, fee, ctx.bumps.config, ctx.bumps.mint_lp)?;
+        ctx.accounts.save_config(seed, fee, &ctx.bumps)?;
         
         Ok(())
     }
 
-    // Add liquidity to receive LP tokens
-    pub fn deposit(ctx: Context<Deposit>, amount: u64, max_x:u64, max_y:u64) -> Result<()> {
-        // max_x and max_y are used for handling slippage
-        // deposit_tokens
-        // mint_lp_token
-        Ok(())
-    }
+    // // Add liquidity to receive LP tokens
+    // pub fn deposit(ctx: Context<Deposit>, amount: u64, max_x:u64, max_y:u64) -> Result<()> {
+    //     // max_x and max_y are used for handling slippage
+    //     // deposit_tokens
+    //     // mint_lp_token
+    //     Ok(())
+    // }
 
-    // Burn LP tokens to withdraw tokens
-    pub fn withdraw(ctx: Context<Withdraw>, amount: u64, min_x: u64, min_y: u64) -> Result<()> {
-        // burn lp tokens
-        // withdraw tokens
+    // // Burn LP tokens to withdraw tokens
+    // pub fn withdraw(ctx: Context<Withdraw>, amount: u64, min_x: u64, min_y: u64) -> Result<()> {
+    //     // burn lp tokens
+    //     // withdraw tokens
         
-        Ok(())
-    }
+    //     Ok(())
+    // }
 
-    // Swap tokens
-    pub fn swap(ctx: Context<Swap>, amount_in: u64, minimum_amount_out: u64, is_x: bool) -> Result<()> {
-        Ok(())
-    }
+    // // Swap tokens
+    // pub fn swap(ctx: Context<Swap>, amount_in: u64, minimum_amount_out: u64, is_x: bool) -> Result<()> {
+    //     Ok(())
+    // }
 
-    pub fn lock(ctx: Context<Lock>, amount: u64) -> Result<()> {
-        Ok(())
-    }
+    // pub fn lock(ctx: Context<Lock>, amount: u64) -> Result<()> {
+    //     Ok(())
+    // }
 
-    pub fn unlock(ctx: Context<Unlock>, amount: u64) -> Result<()> {
-        Ok(())
-    }
+    // pub fn unlock(ctx: Context<Unlock>, amount: u64) -> Result<()> {
+    //     Ok(())
+    // }
 
-    pub fn collect(ctx: Context<Collect>) -> Result<()> {
-        // collect fees
-        Ok(())
-    }
+    // pub fn collect(ctx: Context<Collect>) -> Result<()> {
+    //     // collect fees
+    //     Ok(())
+    // }
 }
 
